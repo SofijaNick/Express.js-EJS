90f671ce27ecfa366c8eaba03c52197b3d1b6c1e
SofijaNick <sofijaa.nevo@gmail.com>
2022-11-25 21:45:02 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const redditData = require('./data.json');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/cats', (req, res) => {
+    const cats = ['Pera', 'Zika', 'Mika', 'Laza'];
+    res.render('cats', { cats })
+})
+
+app.get('/rand', (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1;
+    res.render('random', { num });
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    const data = redditData[subreddit];
+    if(data){
+        res.render('subreddit', {...data});
+    } else {
+        res.render('notfound', {subreddit});
+    }
+})
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+})
